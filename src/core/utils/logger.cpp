4a49df7fc565943df316d46faff94e51a8439806
muhkartal <Mavi_servis@outlook.com>
2025-04-26 09:05:35 +0300

    shgsdfsdf

--- src/core/utils/logger.cpp
+++ src/core/utils/logger.cpp
@@ -60,5 +60,27 @@ const char* Logger::logLevelToString(LogLevel level) const {
     }
 }
 
+void Logger::log(LogLevel level, const char* message) {
+    if (level < current_level_) {
+        return;
+    }
+
+    auto now = std::chrono::system_clock::now();
+    auto time_t_now = std::chrono::system_clock::to_time_t(now);
+    std::tm tm_now{};
+
+#ifdef _WIN32
+    localtime_s(&tm_now, &time_t_now);
+#else
+    localtime_r(&time_t_now, &tm_now);
+#endif
+
+    char timestamp[32];
+    std::strftime(timestamp, sizeof(timestamp), "%Y-%m-%d %H:%M:%S", &tm_now);
+
+    std::lock_guard<std::mutex> lock(mutex_);
+    log_file_ << "[" << timestamp << "] [" << logLevelToString(level) << "] " << message << std::endl;
+}
+
 }
 }
--- src/core/utils/logger.hpp
+++ src/core/utils/logger.hpp
@@ -22,11 +22,11 @@ public:
     static void initialize(const std::string& filename, LogLevel level = LogLevel::INFO);
     static Logger& getInstance();
 
-    template<typename... Args>
-    void log(LogLevel level, const char* format, Args&&... args);
-
     void setLogLevel(LogLevel level);
     LogLevel getLogLevel() const;
+
+    // Non-template function for simple case
+    void log(LogLevel level, const char* message);
 
 private:
     Logger();
@@ -34,9 +34,6 @@ private:
 
     Logger(const Logger&) = delete;
     Logger& operator=(const Logger&) = delete;
-
-    template<typename... Args>
-    std::string formatString(const char* format, Args&&... args);
 
     const char* logLevelToString(LogLevel level) const;
 
@@ -46,55 +43,16 @@ private:
     std::string log_filename_;
     std::ofstream log_file_;
     LogLevel current_level_;
-    mutable std::mutex mutex_;  // Changed to mutable to fix const method issue
+    mutable std::mutex mutex_;  // Must be mutable
 };
 
-// Template implementations need to be in the header file
-template<typename... Args>
-inline void Logger::log(LogLevel level, const char* format, Args&&... args) {
-    if (level < current_level_) {
-        return;
-    }
-
-    auto now = std::chrono::system_clock::now();
-    auto time_t_now = std::chrono::system_clock::to_time_t(now);
-    std::tm tm_now{};
-
-#ifdef _WIN32
-    localtime_s(&tm_now, &time_t_now);
-#else
-    localtime_r(&time_t_now, &tm_now);
-#endif
-
-    char timestamp[32];
-    std::strftime(timestamp, sizeof(timestamp), "%Y-%m-%d %H:%M:%S", &tm_now);
-
-    // Format the message
-    char message_buffer[1024];
-    snprintf(message_buffer, sizeof(message_buffer), format, std::forward<Args>(args)...);
-
-    std::lock_guard<std::mutex> lock(mutex_);
-    log_file_ << "[" << timestamp << "] [" << logLevelToString(level) << "] " << message_buffer << std::endl;
-}
-
-template<typename... Args>
-inline std::string Logger::formatString(const char* format, Args&&... args) {
-    char buffer[1024];
-    int result = snprintf(buffer, sizeof(buffer), format, std::forward<Args>(args)...);
-
-    if (result < 0) {
-        return "Error formatting string";
-    }
-
-    return std::string(buffer, result);
-}
-
-#define LOG_TRACE(...) ::netsentry::utils::Logger::getInstance().log(::netsentry::utils::LogLevel::TRACE, __VA_ARGS__)
-#define LOG_DEBUG(...) ::netsentry::utils::Logger::getInstance().log(::netsentry::utils::LogLevel::DEBUG, __VA_ARGS__)
-#define LOG_INFO(...) ::netsentry::utils::Logger::getInstance().log(::netsentry::utils::LogLevel::INFO, __VA_ARGS__)
-#define LOG_WARNING(...) ::netsentry::utils::Logger::getInstance().log(::netsentry::utils::LogLevel::WARNING, __VA_ARGS__)
-#define LOG_ERROR(...) ::netsentry::utils::Logger::getInstance().log(::netsentry::utils::LogLevel::ERROR, __VA_ARGS__)
-#define LOG_CRITICAL(...) ::netsentry::utils::Logger::getInstance().log(::netsentry::utils::LogLevel::CRITICAL, __VA_ARGS__)
+// Macro definitions for easier logging
+#define LOG_TRACE(message) ::netsentry::utils::Logger::getInstance().log(::netsentry::utils::LogLevel::TRACE, message)
+#define LOG_DEBUG(message) ::netsentry::utils::Logger::getInstance().log(::netsentry::utils::LogLevel::DEBUG, message)
+#define LOG_INFO(message) ::netsentry::utils::Logger::getInstance().log(::netsentry::utils::LogLevel::INFO, message)
+#define LOG_WARNING(message) ::netsentry::utils::Logger::getInstance().log(::netsentry::utils::LogLevel::WARNING, message)
+#define LOG_ERROR(message) ::netsentry::utils::Logger::getInstance().log(::netsentry::utils::LogLevel::ERROR, message)
+#define LOG_CRITICAL(message) ::netsentry::utils::Logger::getInstance().log(::netsentry::utils::LogLevel::CRITICAL, message)
 
 }
 }
